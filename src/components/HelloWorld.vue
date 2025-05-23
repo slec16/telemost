181267ad699a6f682701d60793e9367feaab97f5
kopytov <kopytov.aa@smtech.ru>
2025-05-23 16:39:42 +0300

    init

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,20 @@
+<script setup>
+import { ref } from 'vue'
+
+// defineProps({
+//   msg: String,
+// })
+
+
+</script>
+
+<template>
+  <h1 class="text-3xl font-bold underline bg-red-300">
+    Hello world!
+  </h1>
+  <v-btn>
+    Button
+  </v-btn>
+</template>
+
+
