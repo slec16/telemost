794a6b5311e7f0abca4a2618ededb7a398d448a1
kopytov <kopytov.aa@smtech.ru>
2025-05-23 18:01:56 +0300

    feat: app sceleton

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,47 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
+    import { ref } from 'vue'
+    import HelloWorld from './components/HelloWorld.vue'
+
+    const theme = ref('light')
+
+    function onClick() {
+        theme.value = theme.value === 'light' ? 'dark' : 'light'
+    }
+
+    const drawer = ref(true)
+    const rail = ref(true)
 </script>
 
-<template>
-  <HelloWorld />
-</template>
 
-<style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
-</style>
+<template>
+    <v-app :theme="theme">
+
+        <v-app-bar title="App bar">
+            <v-spacer></v-spacer>
+            <v-btn text="Toggle Theme" slim @click="onClick"></v-btn>
+        </v-app-bar>
+
+        <v-navigation-drawer v-model="drawer" :rail="rail" permanent @click="rail = false">
+            <v-list>
+                <v-list-item prepend-avatar="https://randomuser.me/api/portraits/men/88.jpg"
+                    title="John Leider">
+                    <template v-slot:append>
+                        <v-btn  @click.stop="rail = !rail"><-</v-btn>
+                    </template>
+                </v-list-item>
+            </v-list>
+
+            <v-divider></v-divider>
+
+            <v-list density="compact" nav>
+                <v-list-item prepend-icon="mdi-home-city" value="home">Home</v-list-item>
+                <v-list-item prepend-icon="mdi-account"  value="channels">Channels</v-list-item>
+                <v-list-item prepend-icon="mdi-account-group-outline" value="friends">Friends</v-list-item>
+            </v-list>
+        </v-navigation-drawer>
+        <v-main>
+            main content
+        </v-main>
+
+    </v-app>
+</template>
