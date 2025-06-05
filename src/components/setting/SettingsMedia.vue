e0797773f781f4daaf0b3f1ba0411263b0efd391
kopytov <kopytov.aa@smtech.ru>
2025-06-05 17:50:46 +0300

    feat: check video

--- src/components/setting/SettingsMedia.vue
+++ src/components/setting/SettingsMedia.vue
@@ -27,6 +27,8 @@
                 <v-slider
                     v-model="microVolume"
                     color="blue"
+                    min="0"
+                    max="1"
                 ></v-slider>
             </div>
             <div class="px-7">
@@ -34,6 +36,8 @@
                     <v-slider
                         v-model="soundVolume"
                         color="blue"
+                        min="0"
+                        max="1"
                     ></v-slider>
             </div>
         </div>
@@ -50,11 +54,20 @@
                     v-model="selectedVideoInput"
                 ></v-select>
             </div>
-            <div class="px-7 pt-2">
-                <div class="border rounded-lg w-full h-40 flex items-center justify-center">
-                    <v-btn>
+            <div class="px-7">
+                <p class="mb-2">Предпросмотр</p>
+                <div class="group border rounded-lg w-full min-h-40 flex items-center justify-center">
+                    <video ref="check-video-ref" v-show="checkVideo"></video>
+                    <v-btn v-if="!checkVideo" @click="checkVideoHandler">
                         Проверить камеру
                     </v-btn>
+                    <div v-else class="absolute z-10 invisible group-hover:visible">
+                        <v-btn 
+                            @click="stopCheckVideoHandler"
+                            icon="mdi-close-circle-outline"
+                            color="error"
+                        ></v-btn>
+                    </div>
                 </div>
             </div>
         </div>
@@ -67,12 +80,16 @@
 </template>
 
 <script setup>
-    import { ref, onMounted, watchEffect } from "vue"
+    import { ref, onMounted, watchEffect, useTemplateRef } from "vue"
 
     const promise = ref(false)
 
-    const soundVolume = ref(0)
+    const videoRef = useTemplateRef('check-video-ref')
+
+    const checkVideo = ref(false)
+
     const microVolume = ref(0)
+    const soundVolume = ref(1)
 
     const audioInputDevices = ref([])
     const audioOutputDevices = ref([])
@@ -85,15 +102,92 @@
 
     watchEffect(() => {
 
-        // console.log(selectedaudioInput.value)
-        //в стору
+        // console.log(microVolume.value)
+        // console.log(soundVolume.value)
+        //в стору значения (через троттл)
+
     })
+
+    const checkVideoHandler = async () => {
+        const stream = await getMediaStream( { audio: false, video: true } );
+        console.log(stream)
+            if (stream) {
+            checkVideo.value = true
+            videoRef.value.srcObject = stream;
+            // Optionally, add event listeners for more control:
+            videoRef.value.onloadedmetadata = () => {
+            videoRef.value.play(); // Start playing the video
+            };
+        } else {
+            // Handle the case where the stream could not be obtained, e.g., show an error message
+            console.error("Failed to get stream.");
+        }
+    }
+
+    const stopCheckVideoHandler = () => {
+        if (videoRef.value.srcObject) {
+            checkVideo.value = false
+            const stream = videoRef.value.srcObject;
+            const tracks = stream.getTracks();
+            tracks.forEach(track => track.stop());
+            videoRef.value.srcObject = null; // Clear the source object
+        }
+    }
+ 
+    const getMediaStream = async ( constraints ) => {
+        try {
+            const stream = await navigator.mediaDevices.getUserMedia( constraints );
+            return stream;
+        } catch (err) {
+            console.error( "Error accessing media devices:", err );
+            return null; 
+        }
+
+    }
+
+    // const setupVideo = async() => {
+    //     const stream = await getMediaStream( { audio: false, video: true } );
+
+    //     if( !stream ) return
+
+    //     const videoTracks = stream.getVideoTracks()
+
+    // }
+
+    const setupAudio = async() => {
+        const stream = await getMediaStream( { audio: true, video: false } );
+
+        if( !stream ) return
+
+        const audioTracks = stream.getAudioTracks();
+
+        if (audioTracks.length === 0) {
+            console.warn("No audio tracks found.");
+            return;
+        }
+
+        const audioContext = new window.AudioContext();
+        const source = audioContext.createMediaStreamSource(stream); 
+        const gainNode = audioContext.createGain(); 
+
+        source.connect(gainNode); // Connect the source to the gain node
+        gainNode.connect(audioContext.destination); // Connect the gain node to the output (speakers)
+
+        microVolume.value = gainNode.gain.value //current value state
+
+        stream.getTracks().forEach(track => track.stop());
+    } 
+
+    const setMicroVolume = ( value ) => {
+
+    }
     
 
     const enumerateDevice = async () => {
 
         navigator.mediaDevices.getUserMedia({ audio: true, video: true })
             .then(function(stream) {
+
                 stream.getTracks().forEach(track =>{
                     // console.log(track)
                      track.stop()
@@ -155,6 +249,8 @@
 
     onMounted( () => {
         enumerateDevice()
+        setupAudio()
+        // setupVideo()
     })
         
 </script>
